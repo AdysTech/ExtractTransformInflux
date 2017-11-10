f48ef44bf6bb8af73b7ae12ce2407f91da74885e
mvadu <adarsha.mv@gmail.com>
2017-11-10 07:44:52 -0500

    Add support to binary and epoch output format

--- ExtractTransformInflux.ps1
+++ ExtractTransformInflux.ps1
@@ -1,23 +1,51 @@
+<#
+.SYNOPSIS
+This script allows to extract data stored in an Influx time series measurement into a CSV file.
+.DESCRIPTION
+This script allows to extract data stored in an Influx time series measurement into a CSV file. There is an option to replace existing tag values with new values. The resulting CSV file can be exported back to another Influx Measurement or updaloded to same one, and old series can be dropped.
+Copyright: mvadu@adystech
+license: MIT
+.EXAMPLE
+.\ExtractTransformInflux.ps1 -influxBaseUrl "http://localhost:8086/query?db=TestDB" -measurement "MyMeasurement"  -oldTags "AppName=App1;Release=Beta" -newTags "AppName=MyApplication;Release=Pre_Beta"
+#>
+
 [CmdletBinding()]
 param (
+    #URL for the Influxdb query entry point (usually 8086 port), include the DB name as well e.g. "http://localhost:8086/query?db=InfluxerDB" 
     [parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string] $influxBaseUrl,
     
+    #measurment to query from
     [parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string] $measurement ,
 
+    #List of tags and their values to be replaced. Usage Pattern: <Tag=Value>. Separate multiple tags by a ;. e.g. "AppName=App1;Release=Beta". Optional parameter. 
     [string] $oldTags,
 
+    #List of tags and their values to replace with. Usage Pattern: <Tag=Value>. Separate multiple tags by a ;. e.g. "AppName=MyApplication;Release=Pre_Beta". Mandatory if -oldTags is specified. 
     [string] $newTags,
 
+    #Filters to restrict the points/series returned. Optional, but recommended 
     [string] $additionalFilter,
     
+    #Influx chunk size, defaults to 10000
     [int] $batchSize = 10000,
 
-    [string]$outFile = ".\InfluxDump.csv"
+    #Output file name, defaults to  ".\InfluxDump.csv"
+    [string]$outFile = ".\InfluxDump.csv",
 
+    #output precision, defaults to  seconds
+    [ValidateSet("ns","u","ms","s","m","h")]
+    [string] $precision = "s",
+
+    #output time format : text - logs in local system locale, epoch will use the epoch at $precison, Binary will be the .Net DateTIme Binary representation, defaults to text
+    [ValidateSet("text","epoch","binary")]
+    [string] $outtype = "text",
+
+    #output time format when the -outtype is text, default will be upto micro second precision yyyy-MM-dd-hh.mm.ss.ffffff
+    [string] $timeformat = "yyyy-MM-dd-hh.mm.ss.ffffff"
 )
 
 begin{
@@ -34,34 +62,23 @@ begin{
     if(!$oldTags.EndsWith(";")) { $oldTags = $oldTags + ";" }
     $oldTagSet = New-Object Collections.Generic.List[PSCustomObject]
     $index = 0
-    $([regex] "(?<tag>.*?)=(?<value>.*?);").Matches($oldTags) | foreach {$oldTagSet.Add([PSCustomObject]@{Index=$index++; Tag = $_.groups["tag"].Value ; Value =  $_.groups["value"].Value })}
+    $([regex] "(?<tag>.*?)=(?<value>.*?);").Matches($oldTags) | ForEach-Object {$oldTagSet.Add([PSCustomObject]@{Index=$index++; Tag = $_.groups["tag"].Value ; Value =  $_.groups["value"].Value })}
 
     if(!$newTags.EndsWith(";")) { $newTags = $newTags + ";" }
     $newTagSet = New-Object Collections.Generic.List[PSCustomObject]
     $index = 0
-    $([regex] "(?<tag>.*?)=(?<value>.*?);").Matches($newTags) |  foreach {$newTagSet.Add([PSCustomObject]@{Index=$index++; Tag = $_.groups["tag"].Value ; Value =  $_.groups["value"].Value })}
+    $([regex] "(?<tag>.*?)=(?<value>.*?);").Matches($newTags) |  ForEach-Object {$newTagSet.Add([PSCustomObject]@{Index=$index++; Tag = $_.groups["tag"].Value ; Value =  $_.groups["value"].Value })}
 
     if($newTagSet.Count -ne $oldTagSet.Count){
         Write-Host "Number of Tags to replace do not match with new set of tags provided"
         exit
     }
-    
-  # Function EpochConverter{
-  #     [CmdletBinding()]
-  #     param (
-  #         [parameter(Mandatory=$true)]
-  #         [ValidateNotNullOrEmpty()]
-  #         [long] $epoch
-  #     )
-  #     #(New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0,Utc).ToBinary()
-  #     $origin = [datetime]::FromBinary(5233041986427387904).AddTicks($epoch/100)
-  # }
 }
 
 process{
     $recordsProcessed = 0
 
-    $tagFilter = [string]::Join(" OR ",@($oldTagSet | foreach { "$($_.Tag) = '$($_.Value)'"}))
+    $tagFilter = [string]::Join(" OR ",@($oldTagSet | ForEach-Object { "$($_.Tag) = '$($_.Value)'"}))
     $whereClaus = if ($oldTagSet.count -gt 0 -and -not [string]::IsNullOrEmpty($additionalFilter)){
         "where $tagFilter AND $additionalFilter"
     } elseif ($oldTagSet.count -gt 0) {
@@ -74,9 +91,9 @@ process{
     do{
         Write-Progress -activity "Extracting" -status "Points Processed: $recordsProcessed"
 
-        $query = "$influxBaseUrl&epoch=ns&q=select * from $measurement $whereClaus limit $batchSize offset $recordsProcessed"
+        $query = "$influxBaseUrl&epoch=$precision&q=select * from $measurement $whereClaus limit $batchSize offset $recordsProcessed"
 
-        $response = invoke-webrequest $query
+        $response = invoke-webrequest $query -UseBasicParsing
 
         $influxData = $response.Content | ConvertFrom-Json
 
@@ -87,8 +104,6 @@ process{
         }
         $series = $influxData.results[0].series[0]
 
-        $origin = [datetime]::FromBinary(5233041986427387904)
-        
         for ($row = 0; $row -lt $series.values.Count; $row++){
             $point = [ordered]@{}
             for ($col = 0; $col -lt $series.Columns.Count; $col++) {
@@ -98,9 +113,24 @@ process{
         }
         Write-Progress -activity "Processing" -status "Points Processed: $($recordsProcessed + $row)"
 
-        $points | foreach {$_.Time = $origin.AddTicks($_.Time/100)}
+        if($outtype -ne "epoch") {
+            $origin = (New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0,Utc) #[datetime]::FromBinary(5233041986427387904)
+            switch -Exact ($precision) {
+                "ns" {$points | ForEach-Object {$_.Time = $origin.AddTicks($_.Time/100)}}
+                "u" {$points | ForEach-Object {$_.Time = $origin.AddTicks($_.Time * [TimeSpan]::TicksPerMillisecond * 1000)}}
+                "ms" {$points | ForEach-Object {$_.Time = $origin.AddMilliseconds($_.Time )}}
+                "s" {$points | ForEach-Object {$_.Time = $origin.AddSeconds($_.Time)}}
+                "m" {$points | ForEach-Object {$_.Time = $origin.AddMinutes($_.Time)}}
+                "h" {$points | ForEach-Object {$_.Time = $origin.AddHours($_.Time)}}
+            }
+            switch($outtype) {
+                "text" {$points | ForEach-Object {$_.Time = $_.Time.ToString($timeformat)}}
+                "binary" {$points | ForEach-Object {$_.Time = $_.Time.ToBinary()}}
+            }
+        }
+
         foreach($tag in $oldTagSet){
-            $points | where { $_.($tag.Tag) -eq $tag.Value } | foreach { $_.($tag.Tag) = $newTagSet[$tag.Index].Value}
+            $points | Where-Object { $_.($tag.Tag) -eq $tag.Value } | ForEach-Object { $_.($tag.Tag) = $newTagSet[$tag.Index].Value}
         }
 
         $recordsProcessed = $recordsProcessed + $row
--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # ExtractTransformInflux
-This script allows to extract data stored in an Influx time series measurement into a CSV file. There is an option to replace existing tag values with new values [#3904](https://github.com/influxdata/influxdb/issues/3904). The resulting CSV file can be exported back to another Influx Measurement or updaloded to same one, and old series can be dropped.
+This script allows to extract data stored in an Influx time series measurement into a CSV file. There is an option to replace existing tag values with new values. The resulting CSV file can be exported back to another Influx Measurement or updaloded to same one, and old series can be dropped.
+
+The ideal option would have been to have a simple `update tag value` command in Influxdb, but Influx design does not support such a feature at this time. Ref:  [#3904](https://github.com/influxdata/influxdb/issues/3904). Hence the need for this script.
+
+Copyright: mvadu@adystech
+license: MIT
+
+#### SYNTAX
+    ExtractTransformInflux.ps1 [-influxBaseUrl] <String> [-measurement] <String> [[-oldTags] <String>] [[-newTags] <String>] [[-additionalFilter]
+    <String>] [[-batchSize] <Int32>] [[-outFile] <String>] [[-precision] <String>] [[-outtype] <String>] [[-timeformat] <String>] [<CommonParameters>]
+
+#### PARAMETERS
+#####    -influxBaseUrl <String>
+        URL for the Influxdb query entry point (usually 8086 port), include the DB name as well e.g. "http://localhost:8086/query?db=InfluxerDB"
+
+#####    -measurement <String>
+        measurment to query from
+
+#####    -oldTags <String>
+        List of tags and their values to be replaced. Usage Pattern: <Tag=Value>. Separate multiple tags by a ;. e.g. "AppName=App1;Release=Beta". Optional parameter.
+
+#####    -newTags <String>
+        List of tags and their values to replace with. Usage Pattern: <Tag=Value>. Separate multiple tags by a ;. e.g. "AppName=MyApplication;Release=Pre_Beta". Mandatory if -oldTags is specified.
+
+#####    -additionalFilter <String>
+        Filters to restrict the points/series returned. Optional, but recommended
+
+#####    -batchSize <Int32>
+        Influx chunk size, defaults to 10000
+
+#####    -outFile <String>
+        Output file name, defaults to  ".\InfluxDump.csv"
+
+#####    -precision <String>
+        output precision, defaults to  seconds
+
+#####    -outtype <String>
+        output time format : text - logs in local system locale, epoch will use the epoch at $precison, Binary will be the .Net DateTIme Binary representation, defaults to text
+
+#####    -timeformat <String>
+        output time format when the -outtype is text, default will be upto micro second precision yyyy-MM-dd-hh.mm.ss.ffffff
 
 ### Usage
 ```powershell
